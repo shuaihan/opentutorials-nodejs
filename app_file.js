835e43aa1a1f366acbd1f8da4bcb695994250cae
shuaihan <shuaihan@hanmail.net>
2017-07-12 22:12:18 +0900

    글 목록 만들기

--- app_file.js
+++ app_file.js
@@ -2,13 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const fs = require('fs');
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended : false}));
+
 app.locals.pretty = true;
+
 app.set('views', './views_file');
 app.set('view engine', "jade");
+
 app.get('/topic/new', (req, res) => {
     res.render('new');
 });
+
+app.get('/topic', (req, res) => {
+    fs.readdir('data', (err, files) => {
+        if(err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error'); 
+        }
+        res.render('view', {topics:files});    
+    });
+    
+});
+
 app.post('/topic', (req, res) => {
     let title = req.body.title;
     let description = req.body.description;
@@ -21,6 +37,7 @@ app.post('/topic', (req, res) => {
     } );
  
 });
+
 app.listen(3000, () => {
     console.log('Connected, 3000 port!');
 });
