7117384dc0a074dc37556802bb4ce12b531ccb95
shuaihan <shuaihan@hanmail.net>
2017-07-12 21:59:47 +0900

    라우팅, 본문저장

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # opentutorials-nodejs
+- [github pages](https://shuaihan.github.io/opentutorials-nodejs/)
 - [Online Markdown Editor](http://dillinger.io/)
 - [inflearn](https://www.inflearn.com/course/nodejs-%EA%B0%95%EC%A2%8C-%EC%83%9D%ED%99%9C%EC%BD%94%EB%94%A9/)
 - [NodeJS](https://nodejs.org/ko/)
 - [npm](https://www.npmjs.com/)
 
-# module
+# npm module
 - [uglifyjs](https://www.npmjs.com/package/uglifyjs)
 - [underscore](https://www.npmjs.com/package/underscore)
 - [express](https://www.npmjs.com/package/express) 
 - [jade](http://jadelang.net/)
 - [body-parser](https://www.npmjs.com/package/body-parser)
+- [supervisor](https://www.npmjs.com/package/supervisor)
 
 # etc
 - [Semantic URL](https://en.wikipedia.org/wiki/Semantic_URL)
---
+++ app_file.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const fs = require('fs');
+const app = express();
+app.use(bodyParser.urlencoded({ extended : false}));
+app.locals.pretty = true;
+app.set('views', './views_file');
+app.set('view engine', "jade");
+app.get('/topic/new', (req, res) => {
+    res.render('new');
+});
+app.post('/topic', (req, res) => {
+    let title = req.body.title;
+    let description = req.body.description;
+    fs.writeFile(`data/${title}`, description, (err) => {
+        if(err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error'); 
+        }
+        res.send('Success');
+    } );
+ 
+});
+app.listen(3000, () => {
+    console.log('Connected, 3000 port!');
+});
